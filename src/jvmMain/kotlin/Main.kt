361fba4b129bb5e5bba47f0fe9b0ed297f852157
Andrew Golovashevich <tankist.scratch.p@gmail.com>
2022-09-28 02:27:45 +0300

    Simple http server

--- src/jvmMain/kotlin/Main.kt
+++ src/jvmMain/kotlin/Main.kt
@@ -1,12 +1,19 @@
 package io.github.landgrafhomyak.chatwars.wiki
 
+import com.sun.net.httpserver.HttpServer
+import io.github.landgrafhomyak.chatwars.wiki.http_handlers.MainPageHandler
 import org.sqlite.JDBC
 import org.sqlite.SQLiteConnection
+import java.net.InetSocketAddress
 import java.util.*
 
 object Main {
     @JvmStatic
     fun main(vararg args: String) {
-        SQLiteDatabase(JDBC.createConnection(JDBC.PREFIX + "test1.sqlite", Properties()) as SQLiteConnection)
+        val s = HttpServer.create(InetSocketAddress(8089), 0)
+        s.createContext("/", MainPageHandler)
+        s.executor = null;
+        s.start()
+//        SQLiteDatabase(JDBC.createConnection(JDBC.PREFIX + "test1.sqlite", Properties()) as SQLiteConnection)
     }
 }
